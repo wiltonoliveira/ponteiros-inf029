09e59ee7cacc85a6d7eebdf38078da0d5b1077f0
Wilton Oliveira <oliveirajrwilton@gmail.com>
2018-08-17 01:01:24 -0300

    mudanças na função inserir

--- main-ponteiros.c
+++ main-ponteiros.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdio_ext.h> 
 
 #define TAM_PRINC 10
 
 struct auxiliar {
 	int *vet_aux;
+	int tam_vetor;
+	int topo;
 };
 
 
@@ -12,69 +15,67 @@ struct auxiliar {
 
 int  menu (int operador);
 void asteriscos ();
-int inserir_elemento ();
+//void inserir_elemento (struct auxiliar *elemento);
 
 int main(void) {
 	struct auxiliar principal [TAM_PRINC];
 	int op;
-	
+	int i;
 	int pos;
 	
-	
+	for (i = 0; i < TAM_PRINC; i++)
+		principal[i].topo = 0;
+
+	do {
 	op = menu (op);
-	
-	
+		
 	switch (op) {
 		case 1: {
-			printf ("Informe a posicao da estrutura principal sera alocada: ");
-			scanf ("%d", &pos);
+				printf ("Informe a posicao da estrutura principal sera alocada: ");
+				scanf ("%d", &pos);
+				__fpurge(stdin);
 			
-			
-			break;
-		}
+				if (principal[pos].topo == 0){
+					printf("\nVetor auxiliar na posicao %d nao criado\nInforme o tamanho do vetor: ",pos);
+					scanf ("%d", &principal[pos].tam_vetor);
+					__fpurge(stdin);
+				}
+
+				principal[pos].vet_aux = (int *) malloc (sizeof (struct auxiliar));
+
+				//inserir_elemento (principal[pos].vet_aux);
+				break;
+			}
 		
-		case 2: {
-			
-			break;
 		}
-		
-		case 3: {
-			
-			break;
-		}
-		
-		case 4: {
-			
-			break;
-		}
-		
-		case 5: {
-			
-			break;
-		}
-		
-		case 6: {
-			
-			break;
-		}
-		
-		case 7: {
-			
-			break;
-		}
-	}
+	} while (op != 7);
 	
 }
 
+/*
+void inserir_elemento (struct auxiliar *elemento){
+	int x;
+	printf("Digite o elemento\n");
+	scanf ("%d", &x);
+
+
+}
+*/
+
+
+
+
+
+
 int  menu (int operador){
 	asteriscos (); printf ("MENU"); asteriscos (); printf ("\n");
-	printf ("Inserir elemento\n");
-	printf ("Listar numeros de todas as estruturas\n");
-	printf ("Listar os números ordenados para cada estrutura auxiliar\n");
-	printf ("Listar todos os números de forma ordenada\n");
-	printf ("Excluir um elemento\n");
-	printf ("Aumentar o tamanho de uma estrutura auxiliar\n");
-	printf ("Sair\n");
+	printf ("1 - Inserir elemento\n");
+	printf ("2 - Listar numeros de todas as estruturas\n");
+	printf ("3 - Listar os números ordenados para cada estrutura auxiliar\n");
+	printf ("4 - Listar todos os números de forma ordenada\n");
+	printf ("5 - Excluir um elemento\n");
+	printf ("6 - Aumentar o tamanho de uma estrutura auxiliar\n");
+	printf ("7 - Sair\n");
 	printf ("Digite a operacao desejada:");
 	scanf ("%d", &operador);
 	
@@ -84,6 +85,6 @@ int  menu (int operador){
 void asteriscos (){
 	int i;
 	
-	for (i = 0; i < 5; i++)
+	for (i = 0; i < 6; i++)
 		printf ("*");
 }
