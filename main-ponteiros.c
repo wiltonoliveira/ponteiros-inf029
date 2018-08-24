3557ba03bd77f4c92416ea36ccaeede6646dfaaa
Wilton Oliveira <oliveirajrwilton@gmail.com>
2018-08-23 23:25:12 -0300

    função inserir e listar de todas as estruturas 100% implementadas

--- main-ponteiros.c
+++ main-ponteiros.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include <stdio_ext.h>
+#include <stdio_ext.h>
 
 #define TAM_PRINC 10
 
@@ -13,102 +13,140 @@ struct auxiliar {
 typedef struct auxiliar aux;
 
 
-
 int  menu (int operador);
 void asteriscos ();
 
-void novo_vetor (int *vet, int tam);
+void novo_vetor (int **vet, int tam);
 void novo_elemento (int *vet, int topo);
+void lista_todos (int vet[], int tam);
 
 int main(void) {
 	aux principal [TAM_PRINC];
 	int op;
 	int i;
 	int pos;
-	
-	for (i = 0; i < TAM_PRINC; i++)
-		principal[i].vet_aux = NULL;
-	
+
 	for (i = 0; i < TAM_PRINC; i++)
 		principal[i].topo = 0;
 
+    for (i = 0; i < TAM_PRINC; i++)
+		principal[i].vet_aux = NULL;
+
 	do {
 	op = menu (op);
-		
+
 	switch (op) {
-		case 1: {
+			case 1: {
+				//Inserir elemento
+
 				printf ("Informe a posicao da estrutura principal sera alocada: ");
 				scanf ("%d", &pos);
-				fflush(stdin);
-				//__fpurge (stdin);
-			
+				//fflush(stdin);
+				__fpurge (stdin);
+
 				if (principal[pos].topo == 0){
 					printf("\nVetor auxiliar na posicao %d nao criado\nInforme o tamanho do vetor: ",pos);
 					scanf ("%d", &principal[pos].tam_vetor);
-					fflush(stdin);
-					//__fpurge (stdin);
+					//fflush(stdin);
+					__fpurge (stdin);
 
-					novo_vetor (principal[pos].vet_aux, principal[pos].tam_vetor);
+					novo_vetor (&principal[pos].vet_aux, principal[pos].tam_vetor);
+
+					printf("\nendereco: %p\n" , principal[pos].vet_aux);
+
 					novo_elemento (principal[pos].vet_aux, principal[pos].topo);
-					
+
+					printf("Valor inserido out function: %d\n", principal[pos].vet_aux[principal[pos].topo] );
+
 					principal[pos].topo += 1;
 				}
-				
+
 				else if (principal[pos].topo == principal[pos].tam_vetor){
-						printf ("\n\nVETOR DA POSICAO %d CHEIO\nGostaria de aumentar o valor do vetor auxiliar na posicao %d ?", pos, pos);
+						printf ("\n\nVETOR DA POSICAO %d CHEIO\nGostaria de aumentar o valor do vetor auxiliar na posicao %d ?\n", pos, pos);
 				}
+
+				else if (principal[pos].topo < principal[pos].tam_vetor && principal[pos].topo > 0){
+					novo_elemento (principal[pos].vet_aux, principal[pos].topo);
+					principal[pos].topo += 1;
+				}
+
+				break;
+			}
+
+			case 2: {
+				//Listar numeros de todas as estruturas
+
+				for (i = 0; i < TAM_PRINC; i++){
+					printf("VETOR AUXILIAR DA POSICAO %d:", i);
+					lista_todos (principal[i].vet_aux, principal[i].topo);
+				}
+
+				break;
+			}
+			case 3: {
+				//Listar os numeros ordenados para cada estrutura auxiliar
 				
 				break;
 			}
-		
 		}
 	} while (op != 7);
-	
+
+	return 0;
+
 }
 
 
-void novo_vetor (int *vet, int tam){
-	vet = (int *) malloc (tam * sizeof (int));
+void novo_vetor (int **vet, int tam){
+
+	*vet = (int *) malloc (tam * sizeof (int));
+
 }
 
 void novo_elemento (int *vet, int topo){
-	int elem;
-	
+
+    printf("\nPonteiro: %p\n", vet);
+
 	printf ("\n\nDigite o elemento a ser inserido: ");
-	scanf ("%d", &elem);
-	vet[topo] = elem;
-	fflush(stdin);
-	//__fpurge (stdin);
-	
+
+	scanf ("%d", &vet[topo]);
+
+    printf("Valor inserido in function: %d\n", vet[topo]);
+
+	//fflush(stdin);
+	__fpurge (stdin);
+
 }
 
+void lista_todos (int vet[], int topo){
+	int i;
 
+	for (i = 0; i < topo; i++){
+		printf("%d ", vet[i]);
+	}
+
+	printf("\n");
+}
 
 int  menu (int operador){
 	asteriscos (); printf ("MENU"); asteriscos (); printf ("\n");
 	printf ("1 - Inserir elemento\n");
 	printf ("2 - Listar numeros de todas as estruturas\n");
-	printf ("3 - Listar os números ordenados para cada estrutura auxiliar\n");
-	printf ("4 - Listar todos os números de forma ordenada\n");
+	printf ("3 - Listar os numeros ordenados para cada estrutura auxiliar\n");
+	printf ("4 - Listar todos os numeros de forma ordenada\n");
 	printf ("5 - Excluir um elemento\n");
 	printf ("6 - Aumentar o tamanho de uma estrutura auxiliar\n");
 	printf ("7 - Sair\n");
 	printf ("Digite a operacao desejada:");
 	scanf ("%d", &operador);
-	fflush(stdin);
-	//__fpurge (stdin);
-	
-	system ("cls");
-	
+	//fflush(stdin);
+	__fpurge (stdin);
+
 	return operador;
 }
 
-
-
-
 void asteriscos (){
 	int i;
-	
+
 	for (i = 0; i < 6; i++)
 		printf ("*");
 }
